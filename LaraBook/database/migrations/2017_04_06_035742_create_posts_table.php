eae95e9f2670b59163363df166173cd370a889e9
Hardeep Singh <hardeepphp@yahoo.com>
2017-04-06 09:30:38 +0530

    Posts migration
    Posts migration uploaded

---
+++ LaraBook/database/migrations/2017_04_06_035742_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->increments('user_id'); // who loggedin
+            $table->string('content'); // second person he or she who accepted my request
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
